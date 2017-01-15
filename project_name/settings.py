47b6bb3c234bd4d97f5b6d4863557286d0e9dcfc
Piero Palevsky <ppalevsky@gmail.com>
2017-01-15 14:31:12 -0500

    base template

--- project_name/settings.py
+++ project_name/settings.py
@@ -63,7 +63,7 @@ ROOT_URLCONF = '{{ project_name }}.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
